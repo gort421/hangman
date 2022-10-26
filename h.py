4752e0fa73b897bf0699e03adbf808c01b538ab2
gort421 <113192759+gort421@users.noreply.github.com>
2022-10-26 11:00:01 -0400

    Create h.py

---
+++ h.py
@@ -0,0 +1,19 @@
+## generate word from a list of words
+## track location of each letter
+## ask user to input a letter
+## make a varibale that gives user 7 tries
+## create a while loop to constantly ask user for input until 7 tries are over or user gets the word right
+## if letter guessed is in that word, print location of the letter
+## else, remove 1 from variable tries
+## always check to make sure tries > 0
+## if variable tries is 0, break the while loop
+## else, continue the game
+
+import random
+
+print('Welcome to Hangman!')
+words = ('dog, cat, book, mouse, money, hand, arm, heart, house, lung, rat, bird, coronavirus, python, car, jump, run, apple, taco, Pneumonoultramicroscopicsilicovolcanoconiosis' )
+
+
+
+    
